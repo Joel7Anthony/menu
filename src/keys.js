81db795f36004e8a120f140958aff7dee178a89b
Joel7Anthony <anto777molina777@gmail.com>
2023-08-23 15:40:10 -0500

    footer

--- src/keys.js
+++ src/keys.js
@@ -14,7 +14,7 @@
 const MYSQLHOST = process.env.MYSQLHOST || 'localhost'
 const MYSQLUSER = process.env.MYSQLUSER || 'root'
 const MYSQLPASSWORD = process.env.MYSQLPASSWORD || ''
-const MYSQLDATABASE = process.env.MYSQLDATABASE || 'dijital'
+const MYSQLDATABASE = process.env.MYSQLDATABASE || 'digital'
 const MYSQLPORT = process.env.MYSQLPORT || 3306
 const MYSQL_URI = process.env.MYSQL_URI || ''
 
